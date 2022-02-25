b5f309736d34858665f256a448b32b920928d34d
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-02-25 11:30:11 -0500

    initial project add

---
+++ src/components/Presentation.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Presentation = () => {
+  return (
+    <>
+      <div id="root">
+        <div className="presentation">
+          <iframe
+            className="presentation-iframe"
+            loading="lazy"
+            src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAE5VPkbn7U&#x2F;view?embed"
+            allowFullScreen="allowfullscreen"
+            title="git/github presentation"
+            allow="fullscreen"></iframe>
+        </div>
+        <a
+          href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAE5VPkbn7U&#x2F;view?utm_content=DAE5VPkbn7U&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link"
+          target="_blank"
+          rel="noreferrer">
+          Slidedeck
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Presentation;
---
+++ src/components/common/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Resources</li>
+        <li>Presentation</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
