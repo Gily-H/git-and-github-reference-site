e04c286678635877d1b14d818e3fdc5ac0eeb85c
gilman huang <gilman.huang213@gmail.com>
2022-02-25 11:42:39 -0500

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./styles/App.css";
 import Presentation from "./components/Presentation";
 
 function App() {
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/Presentation.css"
 
 const Presentation = () => {
   return (
